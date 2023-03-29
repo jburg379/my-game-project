e20a6ffaf1da06481f271f668b71df8248407214
jburg379 <118389078+jburg379@users.noreply.github.com>
2023-03-29 15:09:15 -0400

    Add files via upload

---
+++ Project2_Hero's Dungeon Code.cs
@@ -0,0 +1,18 @@
+/************************
+                        The Hero's Dungeon
+                         By Joseph Burgos
+                            CSC 240-10
+                          March 5, 2023
+                            Project 2
+
+About the game:
+You’re a young adventurer that was out on a quest and stepped on a trap that teleported you to the Hero’s Dungeon. 
+You have never been to this dungeon, but you know it’s legend. 
+This is the dungeon which only the Hero of the world can clear. 
+The goal is to exit this dungeon without reaching the boss room. 
+However, the maze at every level makes it hard for you to find the exit that every level of the dungeon has.
+
+Warning:
+The game is an adventure genre game and contains violence (rated: PG-13).
+As such, under 13 should not be playing this game without parental consent and oversight!
+
